3aa37e67dbc7c2601620422758f03a72724a8578
Shristi Bhattarai <shristiitsirhs6@gmail.com>
2025-05-05 07:31:06 +0545

    adding useState

--- src/App.jsx
+++ src/App.jsx
@@ -28,27 +28,53 @@
 
 import React, { useState } from 'react'
 
-const App = () => {
-  const [show,setShow] = useState(false);
+// const App = () => {
+//   const [show,setShow] = useState(false);
 
+//   return (
+//     <div>
+// {
+//   show && (
+//     <div className="box bg-amber-300 w-fit text-white p-2 flex flex-col justify-center items-center">
+//       <h1> Product Name : Laptop</h1>
+//       <h1> Title : Brand New Laptop</h1>
+//       <h1> Description : This is latest laptop in town.</h1>
+//     </div>
+//   )
+// }
+// <button className='bg-blue-500 p-2 rounded-lg ' onClick={() => setShow (!show)}>
+//    {
+// show ? "hide box" : "show box"
+// }
+// </button>
+//     </div>
+//   )
+// }
+
+// export default App
+
+// import React from 'react'
+
+const App = () => {
+  const [count,SetCount] = useState(0)
+  const Increment = ()=> {
+    SetCount (count +1)
+  }
+
+  const Decrement = ()=> {
+    if(count < 1){
+      return;
+    }
+    SetCount (count -1)
+  }
   return (
-    <div>
-{
-  show && (
-    <div className="box bg-amber-300 w-fit text-white p-2 flex flex-col justify-center items-center">
-      <h1> Product Name : Laptop</h1>
-      <h1> Title : Brand New Laptop</h1>
-      <h1> Description : This is latest laptop in town.</h1>
+    <div className='flex justify-center items-center h-screen'>
+      <p className='text-9xl'>{count}</p>
+      <div><button  className='bg-blue-500 text-white rounded-lg p-2 mr-5' onClick={Increment}>Increment</button>
+      <button  className='bg-blue-500 text-white rounded-lg p-2'onClick={Decrement}>Decrement</button>
     </div>
-  )
-}
-<button className='bg-blue-500 p-2 rounded-lg ' onClick={() => setShow (!show)}>
-   {
-show ? "hide box" : "show box"
-}
-</button>
     </div>
-  )
+  );
 }
 
 export default App
